f3e725b6751cf8e6d214959db7ef976dca469c50
Bethany Fort <lnufnu@users.noreply.github.com>
2015-11-20 01:28:42 -0600

    fixed grouping of questions need to take out newline

--- parse.py
+++ parse.py
@@ -1,13 +1,25 @@
+from itertools import islice
+
 with open('questionpool.txt') as f:
     lines = f.readlines()
-    lines = lines[2:]
-    for line in lines:
-        correct_answer = line.split()[1].strip('()')
-        question = f.next()
-        answer_a = f.next().lstrip('A. ')
-        answer_b = f.next().lstrip('B. ')
-        answer_c = f.next().lstrip('C. ')
-        answer_d = f.next().lstrip('D. ')
-    print correct_answer
-    print question
-    print answer_a
+
+line_counter = 2
+offset = 8
+
+# for question_section in islice(lines, 2, 7):
+
+while line_counter < len(lines):
+    these_lines = lines[line_counter:(line_counter+offset)]
+    import ipdb; ipdb.set_trace()
+    for line in these_lines:
+        # correct_answer = question_section[0].split()[1].strip('()')
+        question = line
+        answer_a = line.lstrip('A. ')
+        answer_b = line.lstrip('B. ')
+        answer_c = line.lstrip('C. ')
+        answer_d = line.lstrip('D. ')
+
+    offset += offset
+print correct_answer
+print question
+print answer_a
