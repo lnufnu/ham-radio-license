0edb601255b5c9e6aa5310d474c12fbad1e5ca2c
Bethany Fort <lnufnu@users.noreply.github.com>
2015-11-21 00:29:49 -0600

    fixed newline/carriage return

--- parse.py
+++ parse.py
@@ -1,16 +1,29 @@
 from itertools import islice
 
 with open('questionpool.txt') as f:
+#    import ipdb; ipdb.set_trace()
     lines = f.readlines()
+#    calling .split("\n") on readlines does not work because .split not list method
+#    lines = lines.rstrip('\n') this does not work because rstrip is not list method
+
+
+
+data_set = []
+for line in lines:
+    line = line.rstrip('\n')
+    line = line.rstrip('\r')
+    data_set.append(line)
+
+import ipdb; ipdb.set_trace()
+
 
 line_counter = 2
 offset = 8
 
 # for question_section in islice(lines, 2, 7):
 
-while line_counter < len(lines):
-    these_lines = lines[line_counter:(line_counter+offset)]
-    import ipdb; ipdb.set_trace()
+while line_counter < len(data_set):
+    these_lines = data_set[line_counter:(line_counter+offset)]
     for line in these_lines:
         # correct_answer = question_section[0].split()[1].strip('()')
         question = line
